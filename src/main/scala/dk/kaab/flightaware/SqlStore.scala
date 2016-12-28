0ec4be3f7b07f33f3b78293dae51c9d9c0fa093c
Kasper Fock <kasper@easyspeedy.com>
2016-12-28 22:44:28 +0100

    added log for each sql statement

--- src/main/scala/dk/kaab/flightaware/SqlStore.scala
+++ src/main/scala/dk/kaab/flightaware/SqlStore.scala
@@ -1,25 +1,29 @@
 package dk.kaab.flightaware
 
-import akka.actor.{Actor, ActorLogging, ActorSystem}
+import akka.actor.{Actor, ActorLogging}
 import dk.kaab.flightaware.datatypes.{FlightDetails, FlightResults}
-import scalikejdbc.config.DBs
+import org.slf4j.LoggerFactory
 import scalikejdbc._
+import scalikejdbc.config.DBs
+
+object SqlStore {
+
+  implicit class FlightSampleJdbc(d: FlightDetails) {
 
 
-object SqlStore{
-
-  implicit class FlightSampleJdbc(d:FlightDetails){
-
-
-    def insert()(implicit execution: DBSession):Unit={
+    def insert()(implicit execution: DBSession): Unit = {
       val s = d.sample
-        sql"insert into flightsampler_search (faFlightID, ident, prefix, type, suffix, origin, destination, timeout, timestamp, departureTime, firstPositionTime, arrivalTime, longitude, latitude, lowLongitude, lowLatitude, highLongitude, highLatitude, groundspeed, altitude, heading, altitudeStatus, updateType, altitudeChange, waypoints) values (${s.faFlightID}, ${s.ident}, ${s.prefix}, ${s.`type`}, ${s.suffix}, ${s.origin}, ${s.destination}, ${s.timeout}, ${s.timestamp}, ${s.departureTime}, ${s.firstPositionTime}, ${s.arrivalTime}, ${s.longitude}, ${s.latitude}, ${s.lowLongitude}, ${s.lowLatitude}, ${s.highLongitude}, ${s.highLatitude}, ${s.groundspeed}, ${s.altitude}, ${s.heading}, ${s.altitudeStatus}, ${s.updateType}, ${s.altitudeChange}, ${s.waypoints})".update.apply()
+      sql"insert into flightsampler_search (faFlightID, ident, prefix, type, suffix, origin, destination, timeout, timestamp, departureTime, firstPositionTime, arrivalTime, longitude, latitude, lowLongitude, lowLatitude, highLongitude, highLatitude, groundspeed, altitude, heading, altitudeStatus, updateType, altitudeChange, waypoints) values (${s.faFlightID}, ${s.ident}, ${s.prefix}, ${s.`type`}, ${s.suffix}, ${s.origin}, ${s.destination}, ${s.timeout}, ${s.timestamp}, ${s.departureTime}, ${s.firstPositionTime}, ${s.arrivalTime}, ${s.longitude}, ${s.latitude}, ${s.lowLongitude}, ${s.lowLatitude}, ${s.highLongitude}, ${s.highLatitude}, ${s.groundspeed}, ${s.altitude}, ${s.heading}, ${s.altitudeStatus}, ${s.updateType}, ${s.altitudeChange}, ${s.waypoints})".update.apply()
+      LoggerFactory.getLogger(getClass).info(s"inserted sql : insert into flightsampler_search (faFlightID, ident, prefix, type, suffix, origin, destination, timeout, timestamp, departureTime, firstPositionTime, arrivalTime, longitude, latitude, lowLongitude, lowLatitude, highLongitude, highLatitude, groundspeed, altitude, heading, altitudeStatus, updateType, altitudeChange, waypoints) values (${s.faFlightID}, ${s.ident}, ${s.prefix}, ${s.`type`}, ${s.suffix}, ${s.origin}, ${s.destination}, ${s.timeout}, ${s.timestamp}, ${s.departureTime}, ${s.firstPositionTime}, ${s.arrivalTime}, ${s.longitude}, ${s.latitude}, ${s.lowLongitude}, ${s.lowLatitude}, ${s.highLongitude}, ${s.highLatitude}, ${s.groundspeed}, ${s.altitude}, ${s.heading}, ${s.altitudeStatus}, ${s.updateType}, ${s.altitudeChange}, ${s.waypoints})")
     }
   }
 
 }
-class SqlStore extends Actor with ActorLogging{
+
+class SqlStore extends Actor with ActorLogging {
+
   import SqlStore._
+
   @scala.throws[Exception](classOf[Exception])
   override def preStart(): Unit = {
     super.preStart()
@@ -34,14 +38,13 @@ class SqlStore extends Actor with ActorLogging{
   }
 
   override def receive: Receive = {
-    case f:FlightResults =>
-//      log.warning("Should store message in DB")
+    case f: FlightResults =>
+      //      log.warning("Should store message in DB")
       //todo store in sql db
       DB localTx { implicit session =>
         f.planes.foreach(_.insert())
       }
-
-
+    //log.info(s"Stored ${f.planes.size} samples in sql")
 
   }
 }
