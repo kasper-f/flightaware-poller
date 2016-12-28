6b94cff2efb13ebb49b881c73a4da498aee4d7bc
Kasper Fock <kasper@easyspeedy.com>
2016-12-28 19:57:47 +0100

    cleaned up sql connection

--- src/main/scala/dk/kaab/flightaware/SqlStore.scala
+++ src/main/scala/dk/kaab/flightaware/SqlStore.scala
@@ -14,30 +14,13 @@ object SqlStore{
 
   implicit class FlightSampleJdbc(d:FlightDetails){
 
-    //Getting mySQL connection details from application.conf
-    val config = ConfigFactory.load()
-    val sqlDriver = config.getString("db.default.driver")
-    val sqlUrl = config.getString("db.default.url")
-    val sqlUser = config.getString("db.default.user")
-    val sqlPass = config.getString("db.default.password")
-
-    //println("Dette er min config " + sqlDriver + sqlUser + sqlPass)
 
     def insert():Unit={
       val s = d.sample
 
-      // initialize JDBC driver & connection pool
-      //Class.forName("org.h2.Driver")
-      //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
-
-      Class.forName(sqlDriver)
-      //ConnectionPool.singleton("jdbc:h2:mem:", "mySQLuser", "pass")
-      ConnectionPool.singleton(sqlUrl, sqlUser, sqlPass)
-
-      // ad-hoc session provider on the REPL
-      implicit val session = AutoSession
-
-      sql"insert into flightsampler_search (faFlightID, ident, prefix, type, suffix, origin, destination, timeout, timestamp, departureTime, firstPositionTime, arrivalTime, longitude, latitude, lowLongitude, lowLatitude, highLongitude, highLatitude, groundspeed, altitude, heading, altitudeStatus, updateType, altitudeChange, waypoints) values (${s.faFlightID}, ${s.ident}, ${s.prefix}, ${s.`type`}, ${s.suffix}, ${s.origin}, ${s.destination}, ${s.timeout}, ${s.timestamp}, ${s.departureTime}, ${s.firstPositionTime}, ${s.arrivalTime}, ${s.longitude}, ${s.latitude}, ${s.lowLongitude}, ${s.lowLatitude}, ${s.highLongitude}, ${s.highLatitude}, ${s.groundspeed}, ${s.altitude}, ${s.heading}, ${s.altitudeStatus}, ${s.updateType}, ${s.altitudeChange}, ${s.waypoints})".update.apply()
+      DB localTx { implicit session =>
+        sql"insert into flightsampler_search (faFlightID, ident, prefix, type, suffix, origin, destination, timeout, timestamp, departureTime, firstPositionTime, arrivalTime, longitude, latitude, lowLongitude, lowLatitude, highLongitude, highLatitude, groundspeed, altitude, heading, altitudeStatus, updateType, altitudeChange, waypoints) values (${s.faFlightID}, ${s.ident}, ${s.prefix}, ${s.`type`}, ${s.suffix}, ${s.origin}, ${s.destination}, ${s.timeout}, ${s.timestamp}, ${s.departureTime}, ${s.firstPositionTime}, ${s.arrivalTime}, ${s.longitude}, ${s.latitude}, ${s.lowLongitude}, ${s.lowLatitude}, ${s.highLongitude}, ${s.highLatitude}, ${s.groundspeed}, ${s.altitude}, ${s.heading}, ${s.altitudeStatus}, ${s.updateType}, ${s.altitudeChange}, ${s.waypoints})".update.apply()
+      }
     }
   }
 
